de44bf3d83b68b31079d16896dee3f0b218e1898
Sian Culligan <SianCulligan@gmail.com>
2020-06-09 19:27:08 -0700

    refactored code & updated readme

--- insertionSort.js
+++ insertionSort.js
@@ -1,11 +1,22 @@
+let size = parseInt(process.argv.slice(2));
+let array = [];
+
+for(let i = 0; i < arr.length; i++) {
+  array.push(Math.floor(Math.random() * Math.floor(100)));
+}
+
 function insertionSort(arr) {
-    for(let i = 0; i < arr.length; i++) {
-      let j = i-1;
-      let temp = arr[i];
-      while(j >= 0 && temp < arr[j]) {
-        arr[j + 1] = arr[j];
-        j--
-      }
-      arr[j + 1] = temp
-    } return arr
-  };
+  let i = 0;
+  while (i < arr.length) {
+    let j = i;
+    while(j > 0 && arr[j-1] > arr[j]) {
+      let temp = arr[j-1];
+      arr[j - 1] = arr[j];
+      arr[j] = temp;
+      j--;
+    }
+    i++;
+  }};
+
+  insertionSort(array);
+  console.log(array);
